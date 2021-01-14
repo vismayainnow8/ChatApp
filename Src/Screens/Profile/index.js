d4f8a5f5d01c00869a37d3bbe6226c23d0e80d55
Vismaya_Haridas <vismaya.h123@gmail.com>
2021-01-14 10:37:39 +0530

    Profile page under Settings

--- Src/Screens/Profile/index.js
+++ Src/Screens/Profile/index.js
@@ -1,88 +1,182 @@
-import React from 'react';
-import {View, Text, Image, StatusBar} from 'react-native';
+import React,{useLayoutEffect,useState,useEffect,useRef} from 'react';
+  import { View, Text, Image,Alert,Pressable ,StatusBar } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import ImageCropPicker from 'react-native-image-crop-picker';
+import {StackActions} from '@react-navigation/native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import auth from '@react-native-firebase/auth';
 import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import IconIonicons from 'react-native-vector-icons/Ionicons';
-import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MDIcon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
+import {useDispatch} from 'react-redux';
 
-const Profile = () => {
+
+const Profile = ({ navigation ,route}) => {
+  const _user = auth().currentUser._user;
+  const { uid} = auth().currentUser;
+  const pickerLstRef = useRef(null);
+  const [image, setImage] = useState({});
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  
+  useEffect(  ()=> {
+    console.log('displayName',_user)
+  
+  })
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Profile Info',
+      headerStyle: {
+        backgroundColor: '#075e54',
+        elevation: 0,
+      },
+      headerTintColor: 'white',
+    });
+  });
+
+  const changeProfilePic = () => {
+    pickerLstRef.current.open();
+  };
+
+  const openPicker = () => {
+    pickerLstRef.current.close();
+    ImageCropPicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: true,
+    })
+      .then((image) =>next(image))
+      .catch((error) => console.log('errorddddddddd',error));
+  };
+
+  const openCamera = () => {
+    pickerLstRef.current.close();
+    ImageCropPicker.openCamera({
+      width: 300,
+      height: 300,
+      cropping: true,
+    })
+      .then((image) => next(image) )
+
+       
+      .catch((error) => console.log('errorfffff',error));
+  };
+
+  const next = async (image) => {
+    if (image.path) {
+    setLoading(true);
+      try {
+        const imageStorageRef = storage().ref('images/dp/' + uid + '.jpeg');
+        await imageStorageRef.putFile(image.path);
+        const url = await storage()
+          .ref('images/dp/' + uid + '.jpeg')
+          .getDownloadURL();
+        await firestore()
+          .collection('Users')
+          .doc(uid)
+          .set({ photoURL: url});
+        await auth().currentUser.updateProfile({
+          photoURL: url,
+        });
+        navigation.dispatch(StackActions.replace('WhatsApp'));
+      } catch (error) {
+        console.log('error',error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    else {
+      Alert.alert('', 'Please provide your profile photo ', [
+        {text: 'OK', onPress: () => console.log('Cancel Pressed')},
+      ]);
+    }
+    
+  };
   return (
     <View style={styles.profileContainer}>
       <StatusBar backgroundColor="#075e54" barStyle="light-content" />
       <View style={styles.imageContainer}>
+
+      <View style={styles.imageBackground}>
         <Image
           style={styles.image}
           source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png',
+            uri:_user.photoURL ,
           }}
         />
+         <View style={styles.cameraIconContainer}>
+          <IconFontAwesome5 name="camera" size={25} color="white" onPress={changeProfilePic} />
+        </View>
+        </View>
       </View>
       <View style={styles.textContainer}>
         <View style={styles.editNameContainer}>
           <IconFontAwesome5
             name="user-alt"
-            size={12}
+            size={18}
             color="#075e54"
             style={styles.icon}
           />
-          <View style={styles.itemContainer}>
+          <View>
             <Text style={styles.heading}>Name</Text>
-            <Text style={styles.text}>Vismaya Haridas</Text>
-            <Text style={styles.heading}>
-              This is not your username or pin.This name will be visible to your
-              WhatsApp contacts{' '}
-            </Text>
-          </View>
-          <IconMaterialIcons
+            <View style={styles.itemContainer}>
+          
+            <Text style={styles.text}>{_user.displayName}</Text>
+            {/* <MDIcon
             name="edit"
             size={12}
             color="#075e54"
             style={styles.icon}
-          />
-        </View>
-        <View style={styles.editNameContainer}>
-          <IconIonicons
-            name="information-circle-sharp"
-            size={12}
-            color="#075e54"
-            style={styles.icon}
-          />
-          <View style={styles.itemContainer}>
-            <Text style={styles.heading}>About</Text>
-            <Text style={styles.text}>
-              Be happy and make the world the happiest
-            </Text>
+          /> */}
           </View>
-          <IconMaterialIcons
-            name="edit"
-            size={12}
-            color="#075e54"
-            style={styles.icon}
-          />
+          </View>
         </View>
         <View style={styles.editNameContainer}>
           <IconFontAwesome5
             name="phone-alt"
-            size={12}
+            size={18}
             color="#075e54"
             style={styles.icon}
           />
-          <View style={styles.itemContainer}>
+          <View>
             <Text style={styles.heading}>Phone</Text>
-            <Text style={styles.text}>12345678910</Text>
+          <View style={styles.itemContainer}>
+            <Text style={styles.text}>{_user.phoneNumber}</Text>
           </View>
-          <IconMaterialIcons
-            name="edit"
-            size={12}
-            color="#075e54"
-            style={styles.icon}
-          />
+          </View>
         </View>
 
-        <View style={styles.cameraIconContainer}>
-          <IconFontAwesome5 name="camera" size={25} color="white" />
-        </View>
+       
       </View>
+      <RBSheet
+        ref={pickerLstRef}
+        height={560}
+        customStyles={{
+          container: styles.rbSheet,
+        }}>
+        <Text style={styles.gridHeader}>Profile Photo</Text>
+        <View style={styles.gridContainer}>
+          <Pressable
+            key="photo-camera"
+            onPress={openCamera}
+            style={styles.gridButtonContainer}>
+            <View style={[styles.gridButton]}>
+              <MDIcon name="photo-camera" style={styles.gridIcon} />
+            </View>
+            <Text style={styles.gridLabel}>Take photo</Text>
+          </Pressable>
+          <Pressable
+            key="photo"
+            onPress={openPicker}
+            style={styles.gridButtonContainer}>
+            <View style={[styles.gridButton]}>
+              <MDIcon name="photo" style={styles.gridIcon} />
+            </View>
+            <Text style={styles.gridLabel}>Choose Image</Text>
+          </Pressable>
+        </View>
+      </RBSheet>
     </View>
   );
 };
--- Src/Screens/Profile/styles.js
+++ Src/Screens/Profile/styles.js
@@ -9,53 +9,103 @@ const styles = StyleSheet.create({
     // backgroundColor: "pink"
   },
   imageContainer: {
-    flex: 4,
+    flex: 5,
     alignItems: 'center',
-    justifyContent: 'center',
-    // backgroundColor: "red"
+    justifyContent: 'flex-end',
+    width: consts.ScreenWidth,
+    // backgroundColor: "pink"
   },
   textContainer: {
     flex: 6,
+    width: consts.ScreenWidth,
+    paddingHorizontal: 25,
+    alignSelf:"center",
     // backgroundColor: "green"
   },
-
   image: {
-    height: consts.ScreenWidth * 0.4,
-    width: consts.ScreenWidth * 0.4,
-    borderRadius: consts.ScreenWidth * 0.2,
+    height: consts.ScreenWidth * 0.55,
+    width: consts.ScreenWidth * 0.55,
+    borderRadius: consts.ScreenWidth * 0.4,
+  },
+  imageBackground: {
+    height: consts.ScreenWidth * 0.6,
+    width: consts.ScreenWidth * 0.6,
+    borderRadius: consts.ScreenWidth * 0.5,
+    // backgroundColor: "yellow",
+    alignItems: "center",
+    // justifyContent:"center"
   },
   editNameContainer: {
-    backgroundColor: 'white',
+    // backgroundColor: 'green',
     flexDirection: 'row',
     paddingVertical: 15,
   },
   icon: {
-    paddingRight: 15,
+    paddingHorizontal: 20,
   },
   text: {
     fontSize: consts.textSizes(14),
     color: 'black',
-    paddingBottom: 5,
+    fontWeight:"bold",
+    paddingBottom: 10,
   },
   heading: {
-    fontSize: consts.textSizes(12),
+    fontSize: consts.textSizes(15),
+    paddingBottom:10,
     color: 'grey',
   },
   itemContainer: {
-    borderBottomColor: 'rgba(92,94,94,0.5)',
-    borderBottomWidth: 0.25,
+    flexDirection: "row",
+    // backgroundColor:"pink"
   },
   cameraIconContainer: {
     backgroundColor: '#075e54',
-    position: 'absolute',
+    // position: 'absolute',
     zIndex: 1000,
-    right: consts.ScreenWidth / 3,
-    bottom: consts.ScreenHeight / 1.75,
+    left:40,
+  bottom:50,
     height: 50,
     width: 50,
     borderRadius: 25,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rbSheet: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+    height:consts.ScreenHeight/4,
+    alignItems:"center"
+  },
+  gridHeader: {
+    marginBottom: 20,
+    fontSize: 20,
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    // flexWrap: 'wrap',
+    // justifyContent: "space-evenly",
+    // backgroundColor:"yellow"
+  },
+  gridButtonContainer: {
+    flexBasis: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridButton: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridIcon: {
+    fontSize: 30,
+    color: '#666',
+  },
+  gridLabel: {
+    fontSize: 14,
+    paddingTop: 10,
+    color: '#333',
   },
 });
 export default styles;
